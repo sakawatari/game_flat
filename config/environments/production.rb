1681578d50671f12ec210edc2359bb8deac71961
sakawatari <under.fild1930@gmail.com>
2017-02-20 15:38:22 +0000

    production.rbの設定を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   {
    user_name: ENV['SENDGRID_USERNAME'],
    password: ENV['SENDGRID_PASSWORD'],
-   domain: "13.113.22.115",
+   domain: "heroku.com",
    address: "smtp.sendgrid.net",
    port: 587,
    authentication: :plain,
