53848d9f6c9377ebc868637871afdb6fd152348c
sakawatari <under.fild1930@gmail.com>
2017-02-20 13:50:58 +0000

    deploy.rbの設定を修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.6.0'
 
 # デプロイするアプリケーション名
-set :application, 'game-_flat'
+set :application, 'game_flat'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'https://github.com/sakawatari/game_flat'
